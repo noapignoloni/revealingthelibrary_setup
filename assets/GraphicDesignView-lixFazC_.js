087d438a53783bfe5733ab99bc416e466ffec448
noapignoloni <146346780+noapignoloni@users.noreply.github.com>
2024-04-26 12:34:04 +0200

    Add files via upload

---
+++ assets/GraphicDesignView-lixFazC_.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as s,a as n}from"./index-GGjFFWOU.js";const o={},t=n("p",null,"graphic design",-1),a=[t];function r(_,i){return s(),c("div",null,a)}const l=e(o,[["render",r]]);export{l as default};
